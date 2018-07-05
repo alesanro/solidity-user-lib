21df31c2ab52debd1004582c99dab1b444fdac6b
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-07-05 13:52:13 +0300

    Project: update solcover file

--- .solcover.js
+++ .solcover.js
@@ -2,9 +2,9 @@ module.exports = {
 	copyNodeModules: true,
 	skipFiles: [
 		'migration/Migrations.sol',
-		'helpers/StubStorageManager.sol',
 		'helpers/Mock.sol',
 		'helpers/StorageManager.sol',
 		'helpers/StubRoles2Library.sol',
+		'helpers/BumpedUserBackend.sol',
 	]
   }
