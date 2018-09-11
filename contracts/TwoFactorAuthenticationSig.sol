02443bc3c9628a87055b3cae10a502984c053eca
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-09-11 16:30:09 +0300

    Update TwoFactorAuth to have constant instead of hardcode

--- contracts/TwoFactorAuthenticationSig.sol
+++ contracts/TwoFactorAuthenticationSig.sol
@@ -15,10 +15,12 @@ import "./MultiSig.sol";
 /// as the second owner (at index 1).
 contract TwoFactorAuthenticationSig is MultiSig {
 
+    uint constant TWO_FACTOR_RESERVED_OWNERS_LENGTH = 2;
+
     function _init(address _initiator, address _oracle)
     internal
     {
-        uint _required = 2;
+        uint _required = TWO_FACTOR_RESERVED_OWNERS_LENGTH;
         address[] memory _owners = new address[](_required);
         _owners[0] = _initiator;
         _owners[1] = _oracle;
