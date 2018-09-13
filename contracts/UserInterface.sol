73890f5ace140c9e45250b8ee706a2fa438b2e99
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-09-13 18:46:06 +0300

    Update UserInterface with ThirdParty methods

--- contracts/UserInterface.sol
+++ contracts/UserInterface.sol
@@ -6,19 +6,20 @@
 pragma solidity ^0.4.21;
 
 
-import "./MultiSig.sol";
+import "./ThirdPartyMultiSig.sol";
 import "./UserEmitter.sol";
 
 
 /// @title Interface of a User that is created by UserFactory. Supports multisig operations.
-contract UserInterface is MultiSig, UserEmitter {
+contract UserInterface is ThirdPartyMultiSig, UserEmitter {
     function init(address _oracle, bool _enable2FA) external returns (uint);
     function getUserProxy() external view returns (address);
     function setUserProxy(address _userProxy) external returns (uint);
     function use2FA() external view returns (bool);
     function set2FA(bool _enabled) external returns (uint);
-    function getOracle() external view returns (address);
     function setOracle(address _oracle) external returns (uint);
+    function addThirdPartyOwner(address _owner) external returns (uint);
+    function revokeThirdPartyOwner(address _owner) external returns (uint);
     function updateBackendProvider(address _newBackend) external returns (uint);
     function setRecoveryContract(address _recovery) external returns (uint);
     function getRecoveryContract() external view returns (address);
